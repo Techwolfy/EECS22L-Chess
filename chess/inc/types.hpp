0aed868b686332a10eafe605dfcf82eda82d5183
L16T12 <>
2016-01-20 02:55:31 +0000

    Added header file for enum types (missing from last major revision, oops).

---
+++ chess/inc/types.hpp
@@ -0,0 +1,8 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+//Global enums (beware!)
+enum chess_t {NONE = 0, KING, QUEEN, BISHOP, KNIGHT, ROOK, PAWN};
+enum side_t {NEITHER = 0, WHITE, BLACK, BOTH};
+
+#endif //TYPES_H
