1523a8fb32b10afa74fef8d5700fe83f943532aa
L16T12 <>
2016-01-27 00:43:19 +0000

    Updated Bishop class.

--- chess/src/bishop.cpp
+++ chess/src/bishop.cpp
@@ -28,13 +28,40 @@ Bishop::~Bishop() {
 bool Bishop::checkMove(ChessBoard &board, int fromRow, int fromCol, int toRow, int toCol) {
 	//Diagonal movement
 	if(abs(fromCol - toCol) == abs(fromRow - toRow)) {
+		if(abs(toRow-fromRow)>1){
+			if(fromRow<toRow){
+				if(fromCol<toCol){
+					for(int i=1; i<toRow-fromRow; i++){
+						if(board.getPiece(fromRow+i,fromCol+i)->getCaptured()!=true) return false;
+					}
+				}
+				else if(fromCol>toCol){
+					for(int i=1; i<toRow-fromRow; i++){
+						if(board.getPiece(fromRow+i,fromCol-i)->getCaptured()!=true) return false;
+					}
+				}
+			}
+			else if(fromRow>toRow){
+				if(fromCol<toCol){
+					for(int i=1; i<fromRow-toRow; i++){
+						if(board.getPiece(fromRow-i,fromCol+i)->getCaptured()!=true) return false;
+					}
+				}
+				else if(fromCol>toCol){
+					for(int i=1; i<fromRow-toRow; i++){
+						if(board.getPiece(fromRow-i,fromCol-i)->getCaptured()!=true) return false;
+					}
+				}
+			}
+		}
+		if(board.getPiece(toRow,toCol)->getCaptured()!=true){
+			if(board.getPiece(toRow,toCol)->getSide()==this->side) return false;
+		}
 		return true;
 	}
-
 	//All checks have failed
 	return false;
 }
-
 
 bool Bishop::revertMove(ChessBoard &board, int fromRow, int fromCol, int toRow, int toCol) {
 	//TODO: Implement
