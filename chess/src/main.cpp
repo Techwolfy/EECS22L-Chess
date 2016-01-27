7a9b32f06ff1a4992fe3ce6d4c29c12a4e862e51
L16T12 <>
2016-01-27 22:20:49 +0000

    Fixed game mode menu and cleaned up main.cpp.

--- chess/src/main.cpp
+++ chess/src/main.cpp
@@ -5,66 +5,48 @@
 #include "types.hpp"
 
 int main(int argc, char *argv[]) {
-	Player player,player2;
-	AI ai;
+	Player *player;
+	Player *ai;
 	ChessBoard board;
 	int fromRow = 0, fromCol = 0, toRow = 0, toCol = 0;
-	bool playerMove = true, playerMove2 = false;
+	bool playerMove = true;
 	char playerSide = 'w';
-  int playermode;
+	int gameMode = 0;
 
 	printf("Choose a side (w for white, b for black): ");
 	scanf("%c", &playerSide);
-  printf("Choose mode (pvp(1),pvc(2),cvc(3)): ");
-  scanf("%d", &playermode);
-  
-  if(playermode == 1){
-    if(playerSide == 'w') {
-		player.setSide(WHITE);
-		player2.setSide(BLACK);
-		playerMove = true;
-    playerMove2 = false;
-	  } 
-      else if(playerSide == 'b') {
-		    player.setSide(BLACK);
-		    player2.setSide(WHITE);
-        playerMove = false;
-		    playerMove2 = true;
-	      } 
-          else {
-		        printf("Invalid input! Defaulted to White.\n");
-		        player.setSide(WHITE);
-		        player2.setSide(BLACK);
-		        playerMove = true;
-            playerMove2 = false;
-	        }
-  }
-  
-  if(playermode == 2){
-    if(playerSide == 'w') {
-		player.setSide(WHITE);
-		ai.setSide(BLACK);
-		playerMove = true;
-	  } 
-      else if(playerSide == 'b') {
-		  player.setSide(BLACK);
-		  ai.setSide(WHITE);
-		  playerMove = false;
-	    }   
-        else {
-        printf("Invalid input! Defaulted to White.\n");
-		    player.setSide(WHITE);
-		    ai.setSide(BLACK);
-		    playerMove = true;
-	      }
-  }
-  
-  if (playermode == 3){
-  if(playerSide == 'w') {
+	printf("Available game modes:\n");
+	printf("1. Player vs. Player\n");
+	printf("2. Player vs. AI\n");
+	printf("3. AI vs. AI\n");
+	printf("Choose a game mode: ");
+	scanf("%d", &playermode);
+
+	switch(gameMode) {
+		case 1:
+			player = new Player();
+			ai = new Player();
+			break;
+		case 2:
+			player = new Player();
+			ai = new AI();
+			break;
+		case 3:
+			player = new AI();
+			ai = new AI();
+			break;
+		default:
+			printf("Invalid game mode! Defaulted to Player vs. Player.\n");
+			player = new Player();
+			ai = new AI();
+			break;
+	}
+
+	if(playerSide == 'w' || playerSide == 'W') {
 		player.setSide(WHITE);
 		ai.setSide(BLACK);
 		playerMove = true;
-	} else if(playerSide == 'b') {
+	} else if(playerSide == 'b' || playerSide == 'B') {
 		player.setSide(BLACK);
 		ai.setSide(WHITE);
 		playerMove = false;
@@ -74,8 +56,6 @@ int main(int argc, char *argv[]) {
 		ai.setSide(BLACK);
 		playerMove = true;
 	}
-  }
-
 
 	while(true) {
 		while(board.getWinner() == NEITHER) {
@@ -85,26 +65,13 @@ int main(int argc, char *argv[]) {
 					player.getMove(board, &fromRow, &fromCol, &toRow, &toCol);
 				} while(board.move(player.getSide(), fromRow, fromCol, toRow, toCol) == false);
 				playerMove = false;
-        if(playermode == 1){
-          playerMove2 = true;
-        }
-			} 
-      else if (playerMove2) {
-        //Loops on invalid move
-				do {
-					player2.getMove(board, &fromRow, &fromCol, &toRow, &toCol);
-				} while(board.move(player2.getSide(), fromRow, fromCol, toRow, toCol) == false);
-				playerMove2 = false;
-        playerMove = true;
-			} 
-      else{
+			} else {
 				//Loops on invalid move
 				do {
 					ai.getMove(board, &fromRow, &fromCol, &toRow, &toCol);
 				} while(board.move(ai.getSide(), fromRow, fromCol, toRow, toCol) == false);
 				playerMove = true;
 			}
-
 		}
 
 		//Game over
@@ -116,9 +83,11 @@ int main(int argc, char *argv[]) {
 			printf("Black won!\n");
 		}
 
-		//Prompt for input (new game or quit)
+		//TODO: Prompt for input (new game or quit)
 		break;
 	}
 
+	delete player;
+	delete ai;
 	return 0;
 }
