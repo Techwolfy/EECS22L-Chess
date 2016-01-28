3b21c487e52974f24f8537aace913ed62df4a9d1
L16T12 <>
2016-01-28 02:03:48 +0000

    Fixed King::checkMove, King::move, and King::checkmate functions.

--- chess/src/king.cpp
+++ chess/src/king.cpp
@@ -37,31 +37,26 @@ bool King::checkMove(ChessBoard &board, int fromRow, int fromCol, int toRow, int
 	}
 
 	//New square must not be threatened (king can't move into check)
-	//FIXME: Don't call isThreatened from checkMove, causes infinite loop
-	/*side_t threat = board.isThreatened(toRow, toCol);
-	if(threat != NEITHER && threat != side) {
-		return false;
-	} else {
-		if(isCastling) {
-			if(fromCol < toCol) {
-				threat = board.isThreatened(fromRow, fromCol + 1);
-			} else {
-				threat = board.isThreatened(fromRow, fromCol - 1);
-			}
-			if(threat != NEITHER && threat != side) {
-				return false;
-			}
-		}
-	}*/
+	//NB: Check is performed in King::move, since checking here causes an infinite loop
 
-	//If castling, rook must not have moved either
+	//If castling, rook must not have moved either, and no pieces can be between them
 	if(isCastling) {
 		if(fromCol < toCol) {
+			if(!board.getPiece(fromRow, 3)->getCaptured() || !board.getPiece(fromRow, 2)->getCaptured() || !board.getPiece(fromRow, 1)->getCaptured()) {
+				//Pieces are in the way
+				return false;
+			}
 			if(board.getPiece(fromRow, 0)->getType() != ROOK || board.getPiece(fromRow, 0)->getMoved()) {
+				//Rook has moved
 				return false;
 			}
 		} else {
-			if(board.getPiece(fromRow, 0)->getType() != ROOK || board.getPiece(fromRow, 7)->getMoved()) {
+			if(!board.getPiece(fromRow, 5)->getCaptured() || !board.getPiece(fromRow, 6)->getCaptured()) {
+				//Pieces are in the way
+				return false;
+			}
+			if(board.getPiece(fromRow, 7)->getType() != ROOK || board.getPiece(fromRow, 7)->getMoved()) {
+				//Rook has moved
 				return false;
 			}
 		}
@@ -76,9 +71,27 @@ bool King::move(ChessBoard &board, int fromRow, int fromCol, int toRow, int toCo
 		return false;
 	}
 
+	//Check if king is castling
 	bool isCastling = false;
 	if(fromRow == toRow && abs(fromCol - toCol) == 2) {
 		isCastling = true;
+	}
+
+	//Check if target square or squares in between are threatened
+	side_t threat = board.isThreatened(toRow, toCol);
+	if(threat != NEITHER && threat != side) {
+		return false;
+	} else {
+		if(isCastling) {
+			if(fromCol < toCol) {
+				threat = board.isThreatened(fromRow, fromCol + 1);
+			} else {
+				threat = board.isThreatened(fromRow, fromCol - 1);
+			}
+			if(threat != NEITHER && threat != side) {
+				return false;
+			}
+		}
 	}
 
 	board.getPiece(toRow, toCol)->setCaptured();
@@ -105,6 +118,11 @@ bool King::checkmate(ChessBoard &board, int row, int col) {
 	for(int i = row - 1; i <= row + 1; i++) {
 		for(int j = col - 1; j <= col + 1; j++) {
 			if(i > 0 && i < 8 && j > 0 && j < 8){
+				//For all squares surrounding the king, check if they are unoccupied first
+				if(i != row && j != col && !board.getPiece(i, j)->getCaptured() && board.getPiece(i, j)->getSide() == side) {
+					continue;
+				}
+				//Check if the king and surrounding squares are threatened
 				side_t threat = board.isThreatened(i, j);
 				if(threat == NEITHER || threat == side) {
 					return false;
