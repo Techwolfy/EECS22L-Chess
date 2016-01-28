ae75d4f0e89ce73eb85048ff9911714536028c9a
L16T12 <>
2016-01-28 01:18:12 +0000

    Added new game menu to main().

--- chess/src/main.cpp
+++ chess/src/main.cpp
@@ -7,89 +7,113 @@
 int main(int argc, char *argv[]) {
 	Player *player;
 	Player *ai;
-	ChessBoard board;
+	ChessBoard *board = new ChessBoard();
 	int fromRow = 0, fromCol = 0, toRow = 0, toCol = 0;
 	bool playerMove = true;
 	char playerSide = 'w';
 	int gameMode = 0;
+	char playAgain = 'n';
 
-	printf("Available game modes:\n");
-	printf("1. Player vs. Player\n");
-	printf("2. Player vs. AI\n");
-	printf("3. AI vs. AI\n");
-	printf("Choose a game mode: ");
-	scanf(" %d", &gameMode);
+	while(true) {
+		//Start game
+		printf("Available game modes:\n");
+		printf("1. Player vs. Player\n");
+		printf("2. Player vs. AI\n");
+		printf("3. AI vs. AI\n");
+		printf("Choose a game mode: ");
+		scanf(" %d", &gameMode);
 
-	switch(gameMode) {
-		case 1:
-			player = new Player();
-			ai = new Player();
-			break;
-		case 2:
-			player = new Player();
-			ai = new AI();
-			break;
-		case 3:
-			player = new AI();
-			ai = new AI();
-			break;
-		default:
-			printf("Invalid game mode! Defaulted to Player vs. AI.\n");
-			player = new Player();
-			ai = new AI();
-			break;
-	}
+		//Determine game mode
+		switch(gameMode) {
+			case 1:
+				player = new Player();
+				ai = new Player();
+				break;
+			case 2:
+				player = new Player();
+				ai = new AI();
+				break;
+			case 3:
+				player = new AI();
+				ai = new AI();
+				break;
+			default:
+				printf("Invalid game mode! Defaulted to Player vs. AI.\n");
+				player = new Player();
+				ai = new AI();
+				break;
+		}
 
-	if(gameMode == 1 || gameMode == 2) {
-		printf("Player 1, choose a side (w for white, b for black): ");
-		scanf(" %c", &playerSide);
-	} else {
-		playerSide = 'w';
-	}
+		//Determine player side (if necessary)
+		if(gameMode == 1 || gameMode == 2) {
+			printf("Player 1, choose a side (w for white, b for black): ");
+			scanf(" %c", &playerSide);
+		} else {
+			playerSide = 'w';
+		}
 
-	if(playerSide == 'w' || playerSide == 'W') {
-		player->setSide(WHITE);
-		ai->setSide(BLACK);
-		playerMove = true;
-	} else if(playerSide == 'b' || playerSide == 'B') {
-		player->setSide(BLACK);
-		ai->setSide(WHITE);
-		playerMove = false;
-	} else {
-		printf("Invalid input! Defaulted to White.\n");
-		player->setSide(WHITE);
-		ai->setSide(BLACK);
-		playerMove = true;
-	}
-
-	while(board.getWinner() == NEITHER) {
-		if(playerMove) {
-			//Loops on invalid move
-			do {
-				player->getMove(board, &fromRow, &fromCol, &toRow, &toCol);
-			} while(board.move(player->getSide(), fromRow, fromCol, toRow, toCol) == false);
+		//Set up player and AI objects
+		if(playerSide == 'w' || playerSide == 'W') {
+			player->setSide(WHITE);
+			ai->setSide(BLACK);
+			playerMove = true;
+		} else if(playerSide == 'b' || playerSide == 'B') {
+			player->setSide(BLACK);
+			ai->setSide(WHITE);
 			playerMove = false;
 		} else {
-			//Loops on invalid move
-			do {
-				ai->getMove(board, &fromRow, &fromCol, &toRow, &toCol);
-			} while(board.move(ai->getSide(), fromRow, fromCol, toRow, toCol) == false);
+			printf("Invalid input! Defaulted to White.\n");
+			player->setSide(WHITE);
+			ai->setSide(BLACK);
 			playerMove = true;
 		}
-	}
 
-	//Game over
-	if(board.getWinner() == WHITE) {
-		//White won
-		board.display();
-		printf("White won!\n");
-	} else if(board.getWinner() == BLACK) {
-		//Black won
-		board.display();
-		printf("Black won!\n");
+		//Main game loop
+		while(board->getWinner() == NEITHER) {
+			if(playerMove) {
+				//Loops on invalid move
+				do {
+					player->getMove(*board, &fromRow, &fromCol, &toRow, &toCol);
+				} while(board->move(player->getSide(), fromRow, fromCol, toRow, toCol) == false);
+				playerMove = false;
+			} else {
+				//Loops on invalid move
+				do {
+					ai->getMove(*board, &fromRow, &fromCol, &toRow, &toCol);
+				} while(board->move(ai->getSide(), fromRow, fromCol, toRow, toCol) == false);
+				playerMove = true;
+			}
+		}
+
+		//Game over
+		if(board->getWinner() == WHITE) {
+			//White won
+			board->display();
+			printf("White won!\n");
+		} else if(board->getWinner() == BLACK) {
+			//Black won
+			board->display();
+			printf("Black won!\n");
+		}
+
+		//New game
+		printf("Play again? (y/n): ");
+		scanf(" %c", &playAgain);
+		if(playAgain == 'y') {
+			delete board;
+			board = new ChessBoard();
+			continue;
+		} else if(playAgain == 'n') {
+			printf("Exiting.\n");
+			break;
+		} else {
+			printf("Invalid input! Exiting.\n");
+			break;
+		}
 	}
 
 	delete player;
 	delete ai;
+	delete board;
 	return 0;
 }
