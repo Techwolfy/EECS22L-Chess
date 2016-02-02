69d43bcc02b7ae54643eece48b430ca55a06c5b9
L16T12 <>
2016-02-02 21:51:44 +0000

    Queen is no longer just a rook (oops).

--- chess/src/queen.cpp
+++ chess/src/queen.cpp
@@ -27,79 +27,81 @@ Queen::~Queen() {
 //Functions
 bool Queen::checkMove(ChessBoard &board, int fromRow, int fromCol, int toRow, int toCol) {
 	//Movement within the same row or column
-	if(fromCol != toCol && fromRow != toRow) {
-		return false;
-	}
-	
-	//Check if there are other pieces in the way linearly
-	if(fromCol == toCol) {
-		if(toRow > fromRow) {
-			for(int i = fromRow + 1; i < toRow; i++) {
-				if(!board.getPiece(i, fromCol)->getCaptured()) {
-					return false;
+	if(fromCol == toCol || fromRow == toRow) {
+		//Check if there are other pieces in the way linearly
+		if(fromCol == toCol) {
+			if(toRow > fromRow) {
+				for(int i = fromRow + 1; i < toRow; i++) {
+					if(!board.getPiece(i, fromCol)->getCaptured()) {
+						return false;
+					}
+				}
+			} else {
+				for(int i = fromRow - 1; i > toRow; i--) {
+					if(!board.getPiece(i, fromCol)->getCaptured()) {
+						return false;
+					}
 				}
 			}
 		} else {
-			for(int i = fromRow - 1; i > toRow; i--) {
-				if(!board.getPiece(i, fromCol)->getCaptured()) {
-					return false;
+			if(toCol > fromCol) {
+				for(int i = fromCol + 1; i < toCol; i++) {
+					if(!board.getPiece(fromRow, i)->getCaptured()) {
+						return false;
+					}
+				}
+			} else {
+				for(int i = fromCol - 1; i > toCol; i--) {
+					if(!board.getPiece(fromRow, i)->getCaptured()) {
+						return false;
+					}
 				}
 			}
 		}
-	} else {
-		if(toCol > fromCol) {
-			for(int i = fromCol + 1; i < toCol; i++) {
-				if(!board.getPiece(fromRow, i)->getCaptured()) {
-					return false;
-				}
-			}
-		} else {
-			for(int i = fromCol - 1; i > toCol; i--) {
-				if(!board.getPiece(fromRow, i)->getCaptured()) {
-					return false;
-				}
-			}
-		}
+
+		//Linear movement OK
+		return true;
 	}
 
 	//Diagonal movement
-	if(abs(fromCol - toCol) != abs(fromRow - toRow)) {
-		return false;
-	}
-
-	//Check if there are other pieces in the way diagonally
-	if(fromRow < toRow) {
-		if(fromCol < toCol) {
-			for(int i = 1; i < toRow - fromRow; i++) {
-				if(!board.getPiece(fromRow + i, fromCol + i)->getCaptured()) {
-					return false;
+	if(abs(fromCol - toCol) == abs(fromRow - toRow)) {
+		//Check if there are other pieces in the way diagonally
+		if(fromRow < toRow) {
+			if(fromCol < toCol) {
+				for(int i = 1; i < toRow - fromRow; i++) {
+					if(!board.getPiece(fromRow + i, fromCol + i)->getCaptured()) {
+						return false;
+					}
+				}
+			} else {
+				for(int i = 1; i < toRow - fromRow; i++) {
+					if(!board.getPiece(fromRow + i, fromCol - i)->getCaptured()) {
+						return false;
+					}
 				}
 			}
 		} else {
-			for(int i = 1; i < toRow - fromRow; i++) {
-				if(!board.getPiece(fromRow + i, fromCol - i)->getCaptured()) {
-					return false;
+			if(fromCol < toCol) {
+				for(int i = 1; i < fromRow - toRow; i++) {
+					if(!board.getPiece(fromRow - i, fromCol + i)->getCaptured()) {
+						return false;
+					}
+				}
+			} else {
+				for(int i = 1; i < fromRow - toRow; i++) {
+					if(!board.getPiece(fromRow - i, fromCol - i)->getCaptured()) {
+						return false;
+					}
 				}
 			}
 		}
-	} else {
-		if(fromCol < toCol) {
-			for(int i = 1; i < fromRow - toRow; i++) {
-				if(!board.getPiece(fromRow - i, fromCol + i)->getCaptured()) {
-					return false;
-				}
-			}
-		} else {
-			for(int i = 1; i < fromRow - toRow; i++) {
-				if(!board.getPiece(fromRow - i, fromCol - i)->getCaptured()) {
-					return false;
-				}
-			}
-		}
+
+		//Diagonal movement OK
+		return true;
 	}
 
-	//All checks have passed
-	return true;
+	//All checks have failed
+	return false;
 }
 
 bool Queen::revertMove(ChessBoard &board, int fromRow, int fromCol, int toRow, int toCol) {
