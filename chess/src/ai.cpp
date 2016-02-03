071640bc452cc19d886ca261bd8d837fa9d4700a
L16T12 <>
2016-02-03 23:00:29 +0000

    ai now moves pawn instead of knight

--- chess/src/ai.cpp
+++ chess/src/ai.cpp
@@ -55,34 +55,65 @@ void AI::getMove(ChessBoard &board, int *fromRow, int *fromCol, int *toRow, int 
 	}
  
 //The first move is only for pawns.
-	for(int i = 0; i < 8; i++) {
-		for(int j = 0; j < 8; j++) {
-			if(!board.getPiece(i,j)->getCaptured() && board.getPiece(i, j)->getSide() == side) {   
-				for(int k = 0; k < 8; k++) {
-					for(int l = 0; l < 8; l++) {
-						if(board.checkMove(side, i, j, k, l) && i != k && j != l) {
-							*fromRow = i;
-							*fromCol = j;
-							*toRow = k;
-							*toCol = l;
-							isKing2 = true;
+	if(side==BLACK){
+		for(int i = 0; i < 8; i++) {
+			for(int j = 0; j < 8; j++) {
+				if(!board.getPiece(i,j)->getCaptured() && board.getPiece(i, j)->getSide() == side) {   
+					for(int k = 0; k < 8; k++) {
+						for(int l = 0; l < 8; l++) {
+							if(board.checkMove(side, i, j, k, l) && !(i == k && j == l)) {
+								*fromRow = i;
+								*fromCol = j;
+								*toRow = k;
+								*toCol = l;
+								isKing2 = true;
+								break;
+							}
+						}
+						if(isKing2) {
 							break;
 						}
 					}
-					if(isKing2) {
-						break;
-					}
+				}
+				if(isKing2) {
+					break;
 				}
 			}
+
 			if(isKing2) {
 				break;
 			}
 		}
+	}
+	else {
+		for(int i = 7; i >= 0; i--) {
+			for(int j = 7; j >= 0; j--) {
+				if(!board.getPiece(i,j)->getCaptured() && board.getPiece(i, j)->getSide() == side) {   
+					for(int k = 7; k >= 0; k--) {
+						for(int l = 7; l >= 0; l--) {
+							if(board.checkMove(side, i, j, k, l) && !(i == k && j == l)) {
+								*fromRow = i;
+								*fromCol = j;
+								*toRow = k;
+								*toCol = l;
+								isKing2 = true;
+								break;
+							}
+						}
+						if(isKing2) {
+							break;
+						}
+					}
+				}
+				if(isKing2) {
+					break;
+				}
+			}
 
-		if(isKing2) {
-			break;
+			if(isKing2) {
+				break;
+			}
 		}
 	}
-
 	return;
 }
