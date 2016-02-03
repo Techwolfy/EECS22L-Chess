0a5fbf27b392af0592a02e5fc429ea00ecf84e9a
L16T12 <>
2016-02-03 00:45:07 +0000

    Removed extra call to ChessBoard::display from main().

--- chess/src/main.cpp
+++ chess/src/main.cpp
@@ -89,7 +89,6 @@ int main(int argc, char *argv[]) {
 				} while(board->move(ai->getSide(), fromRow, fromCol, toRow, toCol) == false);
 				playerMove = true;
 			}
-			board->display();
 		}
 
 		//Game over
